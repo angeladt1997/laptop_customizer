926d91983eb3514271ca7b042b57b035005f9265
Angela Thomas <66443127+angeladt1997@users.noreply.github.com>
2020-08-11 15:24:50 -0500

    Update Cart.js

--- src/Cart.js
+++ src/Cart.js
@@ -1,37 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './index.css';
+import './App.css';
+import Summary from './Summary';
+import Total from './Total';
 
-// This object will allow us to
-// easily convert numbers into US dollar values
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
-});
-
-export default class Cart extends React.Component{
-  render(){
-    const summary = Object.keys(this.props.options).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const selectedOption = this.props.options[feature];
-
-      return (
-        <div className="summary__option" key={featureHash}>
-          <div className="summary__option__label">{feature} </div>
-          <div className="summary__option__value">{selectedOption.name}</div>
-          <div className="summary__option__cost">
-            {USCurrencyFormat.format(selectedOption.cost)}
-          </div>
-        </div>
-      );
-    });
-    
-    const total = Object.keys(this.props.options).reduce(
-      (acc, curr) => acc + this.props.options[curr].cost,
-      0
-    );
-    
-    return <div><h2>Your cart</h2>{summary}<div className="summary__total">
-    </div><h2 className="summary__total__label" >Total</h2>
-    <div className="summary__total__value">{USCurrencyFormat.format(total)}</div>
-    </div>
+export default class Cart extends Component {
+  render() {
+    return (
+      <section className="main__summary">
+        <h2>Your cart</h2>
+        <Summary 
+          selected={this.props.selected}
+        />
+        <Total 
+          selected={this.props.selected}
+        />
+      </section>
+    )
   }
 }
