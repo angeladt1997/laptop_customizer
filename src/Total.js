0e9ea1d6a1b0ce405c1af165db31167d114b3e91
Angela Thomas <66443127+angeladt1997@users.noreply.github.com>
2020-08-21 15:31:17 -0500

    Update Total.js

--- src/Total.js
+++ src/Total.js
@@ -1,8 +1,18 @@
-//import React from 'react'
+import React from 'react';
+import USCurrencyFormat from './USCurrencyFormat';
 
-export default function Total(selected){
-  return Object.keys(selected).reduce(
-    (acc, curr) => acc + selected[curr].cost,
-    0
-  );
+const total = (selected) => Object.keys(selected).reduce(
+  (acc, curr) => acc + selected[curr].cost,
+  0
+);
+
+export default function Total(props){
+  return(
+  <div className="summary__total">
+    <div className="summary__total__label">Total</div>
+    <div className="summary__total__value">
+      {USCurrencyFormat.format(total(props.selected))}
+    </div>
+  </div>
+  )
 }
